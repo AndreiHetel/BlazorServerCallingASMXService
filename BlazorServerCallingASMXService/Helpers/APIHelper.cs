212acadbf173a1f9d74886d18cd162f668c04c38
Andrei Hetel (Desktop) <andreihetel@xionagrup.onmicrosoft.com>
2024-05-10 22:03:24 +0300

    Test calls

---
+++ BlazorServerCallingASMXService/Helpers/APIHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+using WSCalculator;
+namespace BlazorServerCallingASMXService.Helpers;
+
+public class APIHelper(string baseURL)
+{
+    private readonly EndpointAddress? _endpointAddress = new(baseURL);
+
+    public async Task<int> AddNumbers(int A, int B )
+    {        
+        using WSCalculator.CalculatorSoapClient client = new(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap, _endpointAddress);
+        return await client.AddAsync(A,B);
+    }
+}
